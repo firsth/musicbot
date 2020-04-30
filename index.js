2a4ec049732369a81dfe6a484b5d6123446a6a20
firsth <37274487+firsth@users.noreply.github.com>
2020-04-30 21:23:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const discord = require('discord.js');
 const client = new discord.Client();
 const search = require('youtube-search');
-console.log(process.env.TOKEN);
 
 const opts = {
     maxResults: 10,
